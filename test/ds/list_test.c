b47750e208fd5e1d069da3485862802dfeda55c3
etaws <etawsz@gmail.com>
2023-03-13 09:34:52 +0800

    add unit testing

--- test/ds/list_test.c
+++ test/ds/list_test.c
@@ -22,3 +22,23 @@ TEST(tree, tree_1) {
 
   tree_clean(root);
 }
+
+TEST(tree, tree_2) {
+
+  int a[] = {1, 0, 2, 3};
+
+  TreeNode* root = tree_create(a, sizeof(a) / sizeof(a[0]));
+
+  int r = 0;
+  int* ra = preorder_traversal(root, &r);
+
+  REQUIRE_EQ(r, 3);
+  REQUIRE_EQ(ra[0], 1);
+  REQUIRE_EQ(ra[1], 2);
+  REQUIRE_EQ(ra[2], 3);
+
+  free(ra);
+  ra = 0;
+
+  tree_clean(root);
+}
